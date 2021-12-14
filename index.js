1dae625f39e0ca313b0b95878210412739e447ed
formalDefault <hurtadoalejandro783@gmail.com>
2021-12-14 03:06:04 -0600

    add conexion a bd

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express(); 
 const cors = require('cors'); 
+ 
+app.use(cors());
+app.use(express.json());
 
 //configuraciones de puerto para express
 app.set('port', process.env.PORT || 4000); 
@@ -11,3 +14,18 @@ app.listen(app.get('port'), () => {
     console.log(`Api iniciada, puerto:${app.get('port')}`);
 }); 
 
+//Conexion a la base de datos mysql
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'salasdejunta',
+    port: 3306
+}); 
+module.exports = db;
+
+db.query("SELECT * FROM reservas", (error, data) => {
+    if(!error) {
+        console.log(data);
+    }
+})
